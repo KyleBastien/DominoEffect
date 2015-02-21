da07d1e4e6e235d106108e857e00db0c34c05a94
Daniel Madatovian <dmadatovian@gmail.com>
2015-02-21 04:29:20 -0800

    Ability to add Domino's to the field

---
+++ Domino Simulator 2015/Assets/DominoCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DominoCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Domino") 
+		{
+			Destroy (gameObject);
+		}
+			
+	}
+}
