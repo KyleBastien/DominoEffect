cdc44317a74a4b2e70b0a73c6f0eb3633bd1ecd0
Alex Pygin <alexpygin@gmail.com>
2015-02-21 17:13:40 -0800

    All the things

---
+++ Domino Simulator 2015/Assets/Scripts/DominoSounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DominoSounds : MonoBehaviour {
+
+	public AudioSource[] src = new AudioSource[2];
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.rigidbody != null) {
+			if (collision.rigidbody.tag == "Domino") {
+				src[0].Play();
+			}
+		} else {
+			src[1].Play ();
+		}
+	}
+
+}
---
+++ Domino Simulator 2015/Assets/Scripts/Pusher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+
+	public float pokeForce;
+	void Update() {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+				if (hit.rigidbody != null)
+					hit.rigidbody.AddForceAtPosition(ray.direction * pokeForce, hit.point);
+	}
+}
